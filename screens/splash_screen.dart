e525f4a4a2dad3e7191d290166a0f0f13af58ee4
“Louie <louiey@naver.com>
2022-06-01 13:10:33 +0900

    login screen 작업 진행 함

--- screens/splash_screen.dart
+++ screens/splash_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_todo_list/screens/list_screen.dart';
+import 'package:flutter_todo_list/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -6,6 +11,33 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  Future<bool> checkLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLogin = prefs.getBool('isLogin') ?? false;
+    print("[*] isLogin : " + isLogin.toString());
+    return isLogin;
+  }
+
+  void moveScreen() async {
+    await checkLogin().then((isLogin) {
+      if (isLogin) {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => ListScreen()));
+      } else {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => LoginScreen()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      moveScreen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +46,7 @@ class _SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('SplashScreen', style: TextStyle(fontSize: 20)),
-            Text('나만의 일정 관리 : TODO 리스트 앱', style: TextStyle(fontSize: 20)),
+            Text('나만의 일정 관리 : TODO 리스트 앱...', style: TextStyle(fontSize: 20)),
           ],
         ),
       ),
